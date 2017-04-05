345afae59291d62a53b9844c0e5ecc68562a0f41
vitezslav.mergl <vitezslav.mergl@gmail.com>
2017-04-05 16:22:39 +0200

    Fix: If any span or a dont exists width is NaN

--- responsive-paginate.js
+++ responsive-paginate.js
@@ -161,8 +161,12 @@
     	    	var width = 0;
     	    	for (var i = 0; i < $container.find("li").length; i++)
     	    	{
-    	    		width += $container.find("li").eq(i).children("a").eq(0).outerWidth();
-    	    		width += $container.find("li").eq(i).children("span").eq(0).outerWidth();
+                    if($container.find("li").eq(i).children("a").eq(0).length > 0){
+                        width += $container.find("li").eq(i).children("a").eq(0).outerWidth();
+                    }
+                    if($container.find("li").eq(i).children("span").eq(0).length > 0){
+                        width += $container.find("li").eq(i).children("span").eq(0).outerWidth();
+                    }
     	    	}
     	    	return width;
     	    }
